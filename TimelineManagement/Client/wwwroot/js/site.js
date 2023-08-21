6af9466f2f8b176df93c2cf115e39f86b30c2f84
doniwramadhan <doniwramadhan@gmail.com>
2023-08-21 14:29:12 +0700

    modal create

--- TimelineManagement/Client/wwwroot/js/site.js
+++ TimelineManagement/Client/wwwroot/js/site.js
@@ -2,3 +2,39 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+function Insert() {
+    var obj = new Object();
+    obj.Name = $("#Name").val();
+    obj.StartDate = $("#StartDate").val();
+    obj.EndDate = $("#EndDate").val();
+    obj.EmployeeGuid = $("#EmployeeGuid").val();
+    obj.TaskName = $("#TaskName").val();
+    obj.Priority = $("#Priority").val();
+    obj.StartDateTask = $("#StartDateTask").val();
+    obj.EndDateTask = $("#EndDateTask").val();
+    $.ajax({
+        url: "https://localhost:7230/api/projects/create-default",
+        type: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify(obj)
+    }).done((result) => {
+        Swal.fire
+            (
+                'Data Has Been Successfuly Inserted',
+                'Success'
+            ).then(() => {
+                location.reload();
+            })
+    }).fail((error) => {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops',
+            text: 'Failed to insert data, Please Try Again',
+        })
+    })
+}
